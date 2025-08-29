2d38811497b6d249892389198eb83f4eb7f075cd
Killberos Tze <killberos.tze@gmail.com>
2025-08-29 21:12:39 +0200

    absorbance calculation

--- DataProcess.py
+++ DataProcess.py
@@ -30,7 +30,7 @@ def convert_speed(speed,oldspeed='m/s',newspeed='km/h'):
     [oldlength,oldtime]=oldspeed.split('/')
     [newlength,newtime]=newspeed.split('/')
     return speed*convert_prefix(oldlength[0:-1],newlength[0:-1])/convert_time(oldtime,newtime)
-    
+
 #mutable approach
 #think about keyword prefix or unit_prefix
 def convert_unit_IHTM(data,newprefix,column):
@@ -39,7 +39,6 @@ def convert_unit_IHTM(data,newprefix,column):
     else:
         data['#data_table'][:,data['#data_summary'][f"{column}_col"]]=convert_value(data['#data_table'][:,data['#data_summary'][f"{column}_col"]], data['#data_summary'][f'{column}_prefix'], newprefix)
     data['#data_summary'][f'{column}_prefix']=newprefix
-
 
 #number of points that you are averaging is 2*n+1 if len(array)-n>i>n
 #n is number of surrounding points (left and right) that you are using for averaging
@@ -56,6 +55,7 @@ def average_IHTM(A,n,column):#n points to average, column to average
     out=copy_IHTM(A)
     out['#data_table'][:,out["#data_summary"][f"{column}_col"]]=mov_average(out['#data_table'][:,out["#data_summary"][f"{column}_col"]],n)
     out['#data_summary'][f"{column}_label"]=out['#data_summary'][f"{column}_label"]+'_avg'
+    out['#data_summary'][f"{column}_smooth_points"]=n
     return out
 
 #it is expected you get [[xi,yi]] and that wavelegth is with same unit
@@ -67,6 +67,19 @@ def numply_at_same_x(A,B):
     bfit = interp1d(B[:,0], B[:,1])
     x=arange(xmin,xmax+xstep,xstep)
     y=afit(x)*bfit(x)
+    out=empty((len(x),2))
+    out[:,0]=x
+    out[:,1]=y
+    return out
+
+def absorb_at_same_x(A,B):
+    xmin=max(min(A[:,0]),min(B[:,0]))
+    xmax=min(max(A[:,0]),max(B[:,0]))
+    xstep=min(A[:,0][1]-A[:,0][0],B[:,0][1]-B[:,0][0])
+    afit = interp1d(A[:,0], A[:,1])
+    bfit = interp1d(B[:,0], B[:,1])
+    x=arange(xmin,xmax+xstep,xstep)
+    y=1-afit(x)-bfit(x)
     out=empty((len(x),2))
     out[:,0]=x
     out[:,1]=y
@@ -86,7 +99,7 @@ def numdiv_at_same_x(A,B):
     return out
 #here you work with your dictionary object
 def multiply_2col_IHTM(A,B):#ihtm A and B have to have same x units and same y units
-    out={}
+    out=copy_IHTM(A)
     out['error']=''
     if A['#data_summary']['tot_col']!=B['#data_summary']['tot_col'] and B['#data_summary']['tot_col']!=2:
         out['error']='wrong number of columns'
@@ -94,15 +107,11 @@ def multiply_2col_IHTM(A,B):#ihtm A and B have to have same x units and same y u
         out['error']='prefixes do not match'
     if A['#data_summary']['x1_prefix']==B['#data_summary']['x1_prefix'] and A['#data_summary']['y1_prefix']==B['#data_summary']['y1_prefix']: 
         out['#data_table']=numply_at_same_x(A['#data_table'],B['#data_table'])
-        #you need deep copy
-        out['#data_summary']={}
-        for keyword,item in A['#data_summary'].items():
-            out['#data_summary'][keyword]=item
         out['#data_summary']['tot_col'],out['#data_summary']['tot_row']=shape(out['#data_table'])
     return out
 
 def divide_2col_IHTM(A,B):
-    out={}
+    out=copy_IHTM(A)
     out['error']=''
     if A['#data_summary']['tot_col']!=B['#data_summary']['tot_col'] and B['#data_summary']['tot_col']!=2:
         out['error']='wrong number of columns'
@@ -110,10 +119,6 @@ def divide_2col_IHTM(A,B):
         out['error']='prefixes do not match'
     if A['#data_summary']['x1_prefix']==B['#data_summary']['x1_prefix'] and A['#data_summary']['y1_prefix']==B['#data_summary']['y1_prefix']: 
         out['#data_table']=numdiv_at_same_x(A['#data_table'],B['#data_table'])
-        #you need deep copy
-        out['#data_summary']={}
-        for keyword,item in A['#data_summary'].items():
-            out['#data_summary'][keyword]=item
         out['#data_summary']['tot_col'],out['#data_summary']['tot_row']=shape(out['#data_table'])
     return out
 
@@ -129,36 +134,21 @@ def copy_IHTM(A):
 def absolute_reflectance_IHTM(Data, Rel_reference, Abs_reference):
     return multiply_2col_IHTM(divide_2col_IHTM(Data, Rel_reference),Abs_reference)
 
-"""    
-
-def absolute_reflectance(Abs, Rel, Data):#Abs, Rel, Data [#data_table] col1 wavelenght col2 values
-	wmin=max(min(Abs['#data_table'][:,0]),min(Rel['#data_table'][:,0]),min(Data['#data_table'][:,0]))
-	wmax=min(max(Abs['#data_table'][:,0]),max(Rel['#data_table'][:,0]),max(Data['#data_table'][:,0]))
-    wstep=min(Abs.wlength[1]-Abs.wlength[0],Rel.wlength[1]-Rel.wlength[0])
-    afit = ipl.interp1d(A.wlength, A.data)
-    bfit = ipl.interp1d(B.wlength, B.data)
-    out.wlength=np.arange(wmin,wmax+wstep,wstep)
-    out.data=afit(out.wlength)*bfit(out.wlength)
+def absorbance_IHTM(A,B):
+    out=copy_IHTM(A)
+    out['error']=''
+    if A['#data_summary']['tot_col']!=B['#data_summary']['tot_col'] and B['#data_summary']['tot_col']!=2:
+        out['error']='wrong number of columns'
+    if A['#data_summary']['x1_prefix']!=B['#data_summary']['x1_prefix'] or A['#data_summary']['y1_prefix']!=B['#data_summary']['y1_prefix']:
+        out['error']='prefixes do not match'
+    if A['#data_summary']['x1_prefix']==B['#data_summary']['x1_prefix'] and A['#data_summary']['y1_prefix']==B['#data_summary']['y1_prefix']:
+        out['#data_table']=absorb_at_same_x(A['#data_table'],B['#data_table'])
+        out['#data_summary']['tot_col'],out['#data_summary']['tot_row']=shape(out['#data_table'])
+        out['#data_summary']['y1_name']='Absorbance'
+        if 'R01' in out['#data_summary']['y1_label']:
+            out['#data_summary']['y1_label']=out['#data_summary']['y1_label'].replace('R01','A')
+        elif 'T01' in out['#data_summary']['y1_label']:
+            out['#data_summary']['y1_label']=out['#data_summary']['y1_label'].replace('T01','A')
+        else:
+            out['#data_summary']['y1_label']=out['#data_summary']['y1_label']+'_A'
     return out
-
-def average_curves(self,data):#data.list in which A.wlength A.data
-        wstep=0
-        wmin=0
-        wmax=np.inf
-        out=container()
-        for item in data:
-            wmin=max(min(item.wlength),wmin)
-            wmax=min(max(item.wlength),wmax)
-            wstep=max((item.wlength[1]-item.wlength[0]),wstep)
-        out.wlength=np.arange(wmin,wmax+wstep,wstep)
-        sum=np.zeros(len(out.wlength))
-        for item in data:
-            fititem=ipl.interp1d(item.wlength, item.data)
-            sum=sum+fititem(out.wlength)
-        out.data=sum/len(data)
-        out.wlength_units=data[0].wlength_units
-        return out
-        #interpolate reference to measured data find Si Sio2 data from 190 to 1100
-        #measured.wlegnthmin:measured.wlength_step:measured.wlength_max #see in TMM
-        #after that just multiply the data
-"""
